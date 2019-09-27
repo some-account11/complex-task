4426ecf85de5394577bd476a649d58aa3b631736
MykhayloDov <myk.nadia@gmail.com>
2019-09-27 21:37:24 +0300

    component created

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-app/src/app/feedback-form/feedback-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-form',
+  templateUrl: './feedback-form.component.html',
+  styleUrls: ['./feedback-form.component.scss']
+})
+export class FeedbackFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
