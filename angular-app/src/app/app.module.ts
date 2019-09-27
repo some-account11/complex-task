b748d11b13047634dabb38b83e7ade23d06c7f94
Vitalii Martyniak <46277409+VitaliyMartyniak@users.noreply.github.com>
2019-09-28 02:12:23 +0300

    finished input and table

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserCatalogComponent } from './user-catalog/user-catalog.component';
+import {FormsModule} from "@angular/forms";
+import {Ng2SearchPipeModule} from "ng2-search-filter";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { UserCatalogComponent } from './user-catalog/user-catalog.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-app/src/app/user-catalog/user-catalog.component.ts
+++ angular-app/src/app/user-catalog/user-catalog.component.ts
@@ -6,7 +6,105 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-catalog.component.scss']
 })
 export class UserCatalogComponent implements OnInit {
-
+  term: string = '';
+  users: any[] = [
+    {
+      "id": 1,
+      "firstName": "Leanne Graham",
+      "lastName": "Bret",
+      "email": "Sincere@april.biz",
+      "password": "1111",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
+    },
+    {
+      "id": 2,
+      "firstName": "Ervin Howell",
+      "lastName": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "password": "2222",
+      "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+          "lat": "-43.9509",
+          "lng": "-34.4618"
+        }
+      },
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+      }
+    },
+    {
+      "id": 3,
+      "firstName": "Clementine Bauch",
+      "lastName": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "password": "3333",
+      "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+          "lat": "-68.6102",
+          "lng": "-47.0653"
+        }
+      },
+      "phone": "1-463-123-4447",
+      "website": "ramiro.info",
+      "company": {
+        "name": "Romaguera-Jacobson",
+        "catchPhrase": "Face to face bifurcated interface",
+        "bs": "e-enable strategic applications"
+      }
+    },
+    {
+      "id": 4,
+      "firstName": "Patricia Lebsack",
+      "lastName": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "password": "4444",
+      "address": {
+        "street": "Hoeger Mall",
+        "suite": "Apt. 692",
+        "city": "South Elvis",
+        "zipcode": "53919-4257",
+        "geo": {
+          "lat": "29.4572",
+          "lng": "-164.2990"
+        }
+      },
+      "phone": "493-170-9623 x156",
+      "website": "kale.biz",
+      "company": {
+        "name": "Robel-Corkery",
+        "catchPhrase": "Multi-tiered zero tolerance productivity",
+        "bs": "transition cutting-edge web services"
+      }
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
