4ad7707c90564e030cecf8599d070e50e7e08f2e
VitaliyMartyniak <vitaliy7martynyak14@gmail.com>
2019-09-27 21:38:00 +0300

    created user-catalog component

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserCatalogComponent } from './user-catalog/user-catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCatalogComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-app/src/app/user-catalog/user-catalog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-catalog',
+  templateUrl: './user-catalog.component.html',
+  styleUrls: ['./user-catalog.component.scss']
+})
+export class UserCatalogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
