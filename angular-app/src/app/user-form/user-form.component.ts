a905525d9430348822870ad7da40ba65f220d370
solodkui <solodkuiA@gmail.com>
2019-09-28 15:09:25 +0300

    user-form v1.0

--- angular-app/src/app/user-form/user-form.component.ts
+++ angular-app/src/app/user-form/user-form.component.ts
@@ -1,15 +1,101 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.scss']
+  styleUrls: ['./user-form.component.scss'],
+  providers: [DataService]
 })
-export class UserFormComponent implements OnInit {
 
-  constructor() { }
+export class UserFormComponent implements OnInit {
+  user: string[] = [];
+
+  constructor(private dataService: DataService){}
 
   ngOnInit() {
   }
 
+  create() {
+    //DOM
+    let firstName         = document.querySelector('#first-name'),
+        lastName          = document.querySelector('#last-name'),
+        email             = document.querySelector('#email'),
+        password          = document.querySelector('#password'),
+
+        firstNameSubtitle = document.querySelector('#first-name-subtitle'),
+        lastNameSubtitle  = document.querySelector('#last-name-subtitle'),
+        emailSubtitle     = document.querySelector('#email-subtitle'),
+        passwordSubtitle  = document.querySelector('#password-subtitle'),
+
+        reg               = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
+
+        checkFirstName    = false,
+        checkLastName     = false,
+        checkEmail        = false,
+        checkPassword     = false;
+
+    // Check First Name
+    if(firstName.value == '') {
+      firstName.style.borderColor = 'red';
+      firstNameSubtitle.innerHTML = 'Enter a first name';
+    } else {
+      firstName.style.borderColor = 'green';
+      firstNameSubtitle.innerHTML = '';
+      checkFirstName = true;
+
+    }
+    // Check Last Name
+    if(lastName.value == '') {
+      lastName.style.borderColor = 'red';
+      lastNameSubtitle.innerHTML = 'Enter a last name';
+    } else {
+      lastName.style.borderColor = 'green';
+      lastNameSubtitle.innerHTML = '';
+      checkLastName = true;
+    }
+    // Check Email
+    if(reg.test(email.value) == false) {
+      email.style.borderColor = 'red';
+      emailSubtitle.innerHTML = 'Please enter the correct email !!!';
+    } else {
+      email.style.borderColor = 'green';
+      emailSubtitle.innerHTML = '';
+      checkEmail = true;
+    }
+    // Check Password
+    if(password.value.length < 6) {
+      password.style.borderColor = 'red';
+      passwordSubtitle.innerHTML = 'Password must be at least 6 characters long !!!';
+    } else {
+      password.style.borderColor = 'green';
+      passwordSubtitle.innerHTML = '';
+      checkPassword = true;
+    }
+
+    // Check All
+    if(checkFirstName && checkLastName && checkEmail && checkPassword) {
+      this.user.push({
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        password: password.value
+      });
+      // Push to Data Service
+      this.dataService.addData(this.user);
+      // Alert
+      setTimeout(() => {
+        alert('New User Added !!!');
+        firstName.value = '';
+        lastName.value = '';
+        email.value = '';
+        password.value = '';
+
+        firstName.style.borderColor = 'black';
+        lastName.style.borderColor = 'black';
+        email.style.borderColor = 'black';
+        password.style.borderColor = 'black';
+      }, 500);
+    }
+  }
 }
