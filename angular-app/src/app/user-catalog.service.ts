1b4f6accae419387a0d6792eb3c2944bf646dce0
Vitalii Martyniak <46277409+VitaliyMartyniak@users.noreply.github.com>
2019-10-02 19:09:58 +0300

    sending for test2

--- angular-app/src/app/user-catalog.service.ts
+++ angular-app/src/app/user-catalog.service.ts
@@ -2,18 +2,25 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Subject} from "rxjs";
 
+interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserCatalogService{
-  public newUserSubject$ = new Subject<any>();
+  public newUserSubject$ = new Subject<User>();
   constructor(public http: HttpClient) { }
 
   getUsers() {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
 
-  sendNewUser(user: any) {
+  sendNewUser(user: User) {
     this.newUserSubject$.next(user);
   }
 
--- angular-app/src/app/user-catalog/user-catalog.component.ts
+++ angular-app/src/app/user-catalog/user-catalog.component.ts
@@ -1,15 +1,23 @@
 import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {UserCatalogService} from "../user-catalog.service";
 
+interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: number;
+}
+
 @Component({
   selector: 'app-user-catalog',
   templateUrl: './user-catalog.component.html',
   styleUrls: ['./user-catalog.component.scss']
 })
+
 export class UserCatalogComponent implements OnInit, OnChanges {
   term: string = '';
-  users: any = [];
-  newUser: any;
+  users: User;
+  newUser: User;
 
   constructor(public userCatalogService: UserCatalogService) {
   }
@@ -17,7 +25,7 @@ export class UserCatalogComponent implements OnInit, OnChanges {
   ngOnInit() {
     this.userCatalogService.getUsers().subscribe((users: any[]) => {
       this.users = users.map(user => {
-        return {'name': user['name'], 'username': user['username'], 'email': user['name'], 'phone': user['phone']};
+        return {'firstName': user['firstName'], 'lastName': user['lastName'], 'email': user['name'], 'password': user['password']};
       });
       console.log(this.users);
     });
