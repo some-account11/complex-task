73b008b4f0825ed3bc4518776e9c113a841549fe
Ihor <kuryloff@gmail.com>
2019-10-04 14:37:59 +0300

    fixed nodemailer copy sending bug

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 const transporter = nodemailer.createTransport({
+        // pool: true,
+        // rateDelta: 200000,
+        // rateLimit: 1,
         host: 'smtp.gmail.com',
         port: 587,
         auth: {
@@ -23,30 +26,32 @@ const transporter = nodemailer.createTransport({
     }
 );
 
-const mailer = async (message) => {
-    await transporter.sendMail(message, (err, info) => {
-        if (err) return console.log(err)
+const mailer = (message) => {
+    transporter.sendMail(message, (err, info) => {
+        if (err) return console.log(err);
+        // transporter.close();
     });
 };
 
-app.post('/data', async (req, res) => {
+app.post('/data', (req, res) => {
     if (!req.body.email) return res.sendStatus(400);
-    const messageToClient = {
+        const messageToClient = {
         to: req.body.email,
         subject: `Feedback info`,
         html: `<h3>Thank you for your feedback. It's very important for us</h3>
                 <p>We received your feedback</p>
-                <span>${req.body.comment}</span>`
+                <span>"${req.body.comment}"</span>`
     };
     const messageToAdmin = {
         to: 'kuryloff@gmail.com',
         subject: `Feedback info`,
         html: `<h3>Feedback received</h3>
                <span><b>From:</b>${req.body.email}</span><br>
-               <span>${req.body.comment}</span>`
+               <span>"${req.body.comment}"</span>`
     };
-    await mailer(messageToClient);
-    await mailer(messageToAdmin);
+    mailer(messageToClient);
+    mailer(messageToAdmin);
+    res.end();
 });
 
 app.listen(PORT, () =>
